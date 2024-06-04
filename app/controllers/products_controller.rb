cbe86004f4d5b44427ed80210468065228be88de
thomas mercuriot <mercuriot.thomas@gmail.com>
2024-06-04 11:50:39 +0200

    Updated Products Controller for New and Create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,16 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to @product, notice: "Product created successfully."
+    else
+      render :new
+    end
   end
 
   def show
