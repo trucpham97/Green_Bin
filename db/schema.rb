2a89b2194aedb2e5291453ab011d253e5be79e3b
thomas mercuriot <mercuriot.thomas@gmail.com>
2024-06-05 10:59:46 +0200

    Added ImageUrl Column To Product Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_145815) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_085730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_04_145815) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
