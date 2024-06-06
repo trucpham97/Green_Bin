e0682eb4d88785562acffa9dc941b2e48b5cf864
thomas mercuriot <mercuriot.thomas@gmail.com>
2024-06-06 12:43:23 +0200

    Added Animation to the Product Card

--- app/javascript/barcode_scanner.js
+++ app/javascript/barcode_scanner.js
@@ -17,12 +17,8 @@ window.addEventListener('load', function () {
         sourceSelect.onchange = () => {
           selectedDeviceId = sourceSelect.value;
         };
-
-        // const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-        // sourceSelectPanel.style.display = 'block'
       }
 
-      // Start scanning as soon as the page loads
       codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
         if (result) {
           if (this.lastResultCode === result.text) return;
@@ -36,22 +32,21 @@ window.addEventListener('load', function () {
 
                 const response = await fetch(`https://world.openfoodfacts.org/api/v0/product/${result}.json`);
                 const data = await response.json();
+
                 console.log("Your product is :", data.product.product_name);
-                // document.getElementById('product-id').textContent = data.product.product_name
                 console.log("image", data.product.image_url);
-                // document.getElementById('product-images').innerHTML = '';
-                // document.getElementById('product-images').insertAdjacentHTML('beforeend', `<img src="${data.product.image_url}" alt="product image">`);
+
                 // Fill Form with product data
                 document.getElementById('product_name').value = data.product.product_name;
                 document.getElementById('product_image_url').value = data.product.image_url;
+
                 // Form Auto-Submit
                 document.getElementById('product-form').submit();
                 document.getElementById('product-card').innerHTML = '';
-                document.getElementById('product-card').insertAdjacentHTML('beforeend', `<div style="background-color: red">${data.product.product_name} <br> <img src="${data.product.image_url}"> </div>`);
+
                 // Custom Event for Stimulus in product_controller.js (ask Thomas for help if needed)
                 const newProduct = { name: data.product.product_name, imageUrl: data.product.image_url };
                 document.dispatchEvent(new CustomEvent('product:created', { detail: { product: newProduct } }));
-
 
             } catch (error) {
                 console.error('Erreur lors de la récupération du produit:', error);
