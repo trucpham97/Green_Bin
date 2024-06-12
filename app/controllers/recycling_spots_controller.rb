fb2f9e6fb87733de1e52e732c7e0a011ece612ff
Thanh Truc Pham <truc.tpham97@gmail.com>
2024-06-12 15:49:06 +0200

    updated seed with tags for product
    Signed-off-by: Thanh Truc Pham <truc.tpham97@gmail.com>

--- app/controllers/recycling_spots_controller.rb
+++ app/controllers/recycling_spots_controller.rb
@@ -1,15 +1,20 @@
 class RecyclingSpotsController < ApplicationController
-  before_action :set_product, only: [:index]
+  # before_action :set_product, only: [:index]
   layout 'just_no_navbar', only: [:index, :list]
   def index
-    @recycling_spots = RecyclingSpot.tagged_with(@product.tag_list, any: true)
+    @product = Product.find(params[:product_id])
+    @recycling_spots = find_recycling_spots_by_tags(@product.tag_list)
     set_markers
   end
 
   def list
     # @recycling_spots = RecyclingSpot.near([current_user.latitude, current_user.longitude], 2)
     # @recycling_spots.concat(RecyclingSpot.near("Place Jean Macé, Lyon", 2))
-    @recycling_spots = RecyclingSpot.near("20 rue des Capucins, Lyon", 2)
+    # @recycling_spots = find_nearby_recycling_spots("20 rue des Capucins, Lyon", 1.1)
+    location = "20 rue des Capucins, Lyon"
+    @recycling_spots = Rails.cache.fetch("nearby_recycling_spots/#{location.parameterize}", expires_in: 24.hours) do
+      find_nearby_recycling_spots(location, 1)
+    end
     set_markers
   end
 
@@ -18,17 +23,27 @@ class RecyclingSpotsController < ApplicationController
   end
 
   private
+
   def set_product
     @product = Product.find(params[:product_id])
   end
 
+  def find_recycling_spots_by_tags(tags)
+    @find_recycling_spots_by_tags ||= RecyclingSpot.tagged_with(tags, any: true)
+  end
+
+  def find_nearby_recycling_spots(location, distance)
+    @find_nearby_recycling_spots ||= RecyclingSpot.near(location, distance)
+  end
+
   def set_markers
+    return unless @recycling_spots.present?
     @markers = @recycling_spots.map do |recycling_spot|
       {
         lat: recycling_spot.latitude,
         lng: recycling_spot.longitude,
-        info_window_html: render_to_string(partial: "shared/info_window", locals: {recycling_spot: recycling_spot}),
-        marker_html: render_to_string(partial: "shared/marker", locals: {recycling_spot: recycling_spot})
+        info_window_html: render_to_string(partial: "shared/info_window", locals: {recycling_spot: recycling_spot}, cache: true),
+        marker_html: render_to_string(partial: "shared/marker", locals: {recycling_spot: recycling_spot}, cache: true)
       }
     end
   end
